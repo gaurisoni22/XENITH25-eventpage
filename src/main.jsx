459733df528f366c8c1b9c048e3b209254f1f2cd
gaurisoni22 <167605198+gaurisoni22@users.noreply.github.com>
2024-12-29 03:20:34 +0530

    added snowfall effect on whole bg

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Snowfall from 'react-snowfall'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
+    <Snowfall
+  style={{
+    position: 'fixed',
+    width: '100vw',
+    height: '100vh',
+  }}
+/>
     <App />
+
   </StrictMode>,
 )
