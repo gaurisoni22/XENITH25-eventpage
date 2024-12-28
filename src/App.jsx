7c9209d28fe9bdffa78ab17c651611d0c982857f
Apoorv Mittal <apoorvmitthu@gmail.com>
2024-12-28 16:26:16 +0530

    center events

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,20 @@ function App() {
 
   return (
     <div className='main'>
-    <div className='toppart'><h1><center>EVENTS</center></h1></div>
-    <div className='content'>
-      <div className='mainbox'>
-        <div className=' box '>eureka</div>
-        <div className=' box '>eureka</div>
-        <div className=' box '>eureka</div>
-
-
+      <div className='toppart'>
+        <img src={icetopbg} alt='icetopbg' className='icewidth' />
+        <div className="text-overlay">EVENTS</div>
       </div>
-    </div>
+      
+      <div className='content'>
+        <div className='mainbox'>
+          <div className=' box '>eureka</div>
+          <div className=' box '>eureka</div>
+          <div className=' box '>eureka</div>
+        </div>
+      </div>
 
-     <div className='bottompart'></div> 
+      <div className='bottompart'></div> 
      
 
     </div>
